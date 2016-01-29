f97787f2b07155799f0ca08ab24e23d486cd57dc
Julien254 <juliensfr254@gmail.com>
2016-01-29 11:03:58 +0100

    test opacity navbar (non concluant)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// animation scroll navbar
 $('a[href^="#"]').click(function(){
 	var mdi4 = $(this).attr("href");
 
@@ -5,4 +6,25 @@ $('a[href^="#"]').click(function(){
 		scrollTop:$(mdi4).offset().top
 	}, 'slow');
 	return false;
+});
+
+//Transparence navbar
+
+var opacity = 0.5;
+var lastScrollTop = 0;
+$(window).scroll(function(){
+    var st = $(this).scrollTop();
+    if(st == 0)
+        $('#mainNav').css('opacity','0.5');
+
+    if (opacity < 1 && (st > lastScrollTop)){
+       $('#mainNav').css('opacity','+=0.1');
+       opacity += 0,1;
+    }
+    else if(opacity > 0.5)
+    {
+       $('#mainNav').css('opacity','-=0.1');
+        opacity -= 0.1;
+    }
+    lastScrollTop = st;
 });
