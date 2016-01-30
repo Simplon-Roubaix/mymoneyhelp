aa4be89a880d588d688a29264133facb43982ffc
Julien254 <juliensfr254@gmail.com>
2016-01-30 21:32:21 +0100

    add script for anchor in main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+  $('a[href^="#"]').click(function(){
+ 	var mdi4 = $(this).attr("href");
+
+ 	$('html, body').animate({
+ 		scrollTop:$(mdi4).offset().top
+ 	}, 'slow');
+ 	return false;
+ });
 //Transparence navbar
 
 var navEl = document.getElementById("mainNav");
